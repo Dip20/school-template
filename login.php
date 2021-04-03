f14463d0d98dbce401ec5f720ba3ca6993608f60
Dip20 <info.dip.sarkar@gmail.com>
2021-04-03 18:23:03 +0530

    test 2 and nav update

--- login.php
+++ login.php
@@ -6,7 +6,6 @@
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <!-- Bootstrap CSS -->
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
       <!-- <link rel="icon" href="img/guaranteed.ico"> -->
       <title>ABCD School - Login</title>
       <!-- custom css -->
@@ -23,37 +22,41 @@
    </head>
    <body>
 
-      <!-- nav starts here -->
-      <nav class="navbar navbar-expand-lg navbar-light bg-custom" id="nav-bar">
-         <a class="brand text-decoration-none" href="index"> <i class="fas fa-graduation-cap rotate"></i>ABCD School</a>
-         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-         <i class="fa fa-bars text-white"></i>
-         </button>
-         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mx-auto">
-               <li class="nav-item">
-                  <a class="nav-link" href="index">Home <span class="sr-only">(current)</span></a>
-               </li>
-               <li class="nav-item">
-                  <a class="nav-link" href="about">About</a>
-               </li>
-               <li class="nav-item ">
-                  <a class="nav-link" href="contact">Contact</a>
-               </li>
-               <li class="nav-item">
-                  <a class="nav-link" href="notice">Notice Board</a>
-               </li>
-               <li class="nav-item">
-                  <a class="nav-link" href="gallery">Gallery</a>
-               </li>
-            </ul>
-            <div class="form-inline my-2 my-lg-0">
-               <button class="btn my-2 my-sm-0 btn-light" type="button"   data-toggle="modal" data-target="#exampleModal">Login</button>
-               <a class="btn  my-2 my-sm-0  ml-2 btn-outline-light" href="register">Register</a>
-            </div>
-         </div>
-      </nav>
-      <!-- nav ends here -->
+     <!-- nav starts here -->
+     <nav class="navbar navbar-expand-lg navbar-light bg-custom" id="nav-bar">
+        <!-- <a class="brand text-decoration-none" href="index"> <i class="fas fa-graduation-cap rotate"></i>ABCD School</a> -->
+        <img src="img/logo.png" class="image-fluid" alt="" width="120px">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fa fa-bars text-white"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+           <ul class="navbar-nav mx-auto">
+              <li class="nav-item active">
+                 <a class="nav-link" href="index">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                 <a class="nav-link" href="about">About</a>
+              </li>
+              <li class="nav-item">
+                 <a class="nav-link" href="contact">Contact</a>
+              </li>
+              <li class="nav-item">
+                 <a class="nav-link" href="notice">Notice Board</a>
+              </li>
+              <li class="nav-item">
+                 <a class="nav-link" href="gallery">Gallery</a>
+              </li>
+              <li class="nav-item">
+                 <a class="nav-link" href="gallery">Fee Structure</a>
+              </li>
+           </ul>
+           <div class="form-inline my-2 my-lg-0">
+              <button class="btn my-2 my-sm-0 btn-light" type="button"   data-toggle="modal" data-target="#exampleModal">Login</button>
+              <a class="btn  my-2 my-sm-0  ml-2 btn-outline-light" href="register">Register</a>
+           </div>
+        </div>
+     </nav>
+     <!-- nav ends here -->
 
       <!-- login section starts here -->
       <section id="login" >
