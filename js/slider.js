982264280e498635c78d77964e029d78288080f6
Dip20 <info.dip.sarkar@gmail.com>
2021-04-03 16:42:00 +0530

    project ini

---
+++ js/slider.js
@@ -0,0 +1,22 @@
+// slider js
+   $('.owl-carousel').owlCarousel({
+   loop:true,
+   margin:10,
+   nav:false,
+   autoplay:true,
+   autoplayTimeout:2500,
+   dots:false,
+   responsiveClass:true,
+   responsive:{
+   0:{
+       items:1
+   },
+   600:{
+       items:1,
+       nav:true,
+   },
+   762:{
+       items:3
+   }
+   }
+   });
